7ca4ded2be66b9cc525f3495e3deee7f2dbffa04
cwellage <cwellage@yahoo.com>
2023-06-09 19:31:28 +0530

    azure connectionstring

--- DataLayer/MyDB.cs
+++ DataLayer/MyDB.cs
@@ -23,7 +23,8 @@ namespace DataLayer
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            builder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = chatura; Integrated Security = True", providerOptions => providerOptions.CommandTimeout(60)).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            builder.UseSqlServer("Server=tcp:cwellageserver.database.windows.net,1433;Initial Catalog=Chatura;Persist Security Info=False;User ID=cwellage;Password=Aravinda1985#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;", providerOptions => providerOptions.CommandTimeout(60)).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            // builder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = chatura; Integrated Security = True", providerOptions => providerOptions.CommandTimeout(60)).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         }
     }
 }
